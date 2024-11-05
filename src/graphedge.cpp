45589fa762fdd0ba60610165ff919afeac0b31b4
rodavi <avipe.rojo@gmail.com>
2024-11-05 10:19:09 -0600

    Add files via upload
    Final revision

---
+++ src/graphedge.cpp
@@ -0,0 +1,24 @@
+#include "graphnode.h"
+#include "graphedge.h"
+
+GraphEdge::GraphEdge(int id)
+{
+    _id = id;
+}
+
+void GraphEdge::SetChildNode(GraphNode *childNode)
+//void GraphEdge::SetChildNode(std::unique_ptr<GraphNode> childNode)
+{
+    _childNode = childNode;
+}
+
+void GraphEdge::SetParentNode(GraphNode *parentNode)
+//void GraphEdge::SetParentNode(std::unique_ptr<GraphNode> parentNode)
+{
+    _parentNode = parentNode;
+}
+
+void GraphEdge::AddToken(std::string token)
+{
+    _keywords.push_back(token);
+}
